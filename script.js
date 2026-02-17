6b6ffb701783794e0fe7032ec630900ac1cf646d
Vinaynetha632 <thiramdasuvinay@gmail.com>
2026-02-17 23:05:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let element = document.getElementById("level")
+let count = 0;
+while(element! == null){
+	count++;
+	element = element.parentElement;
+}
+	alert("The level of the element is: " + count);
